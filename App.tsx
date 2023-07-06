5685baf9fc228cc197563355e6909fdfa5fdf469
Daniel Oluwadare <oluwadare.daniel21@gmail.com>
2023-07-06 09:40:07 -0500

    commit beofore pushing to remote

--- App.tsx
+++ App.tsx
@@ -2,26 +2,29 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import DiseaseSelect from './src/components/DiseaseSelect';
 import Calculate from './src/components/Calculate';
 import StagingValues from './src/components/StagingValues';
-
+import {StyleSheet } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-
-
-
- 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: styles.header,
+           headerTintColor: '#fff',
+           headerTitleStyle: styles.headerTitle,
+          cardStyle: styles.container,
+        }}
+      >
         <Stack.Screen
           name="DiseaseSelect"
           component={DiseaseSelect}
           options={{title: 'Select Disease Site'}}
+         
         />
           <Stack.Screen
           name="StagingValues"
@@ -42,11 +45,20 @@ const App = () => {
 export default App;
 
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    margin: 10,
+    color: 'black',
+  },
+
+  header: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  headerTitle: {
+    color: 'white',
+  }
+});
--- src/components/StagingValueRow.tsx
+++ src/components/StagingValueRow.tsx
@@ -1,125 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import DropDownPicker from 'react-native-dropdown-picker';
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 
-const StagingValueRow = ({ prop=null}) => {
-    const [open, setOpen] = useState(false);
-    const [dropdownValue, setDropdownValue] = useState(null);
-    const [items, setItems] = useState([
-        { label: 'loading...', value: '' },
-      
-    ]);
+const StagingValueRow = ({ prop = null,handleStagingCriteriaSelect }) => {
 
-    useEffect(() => {
-        if (prop != null) {
-          setItems(
-            prop.ValueDescList.map((item,index) => ({
-              label: item.Descr,
-              value: item.ValidValue,
-              key:index
-            }))
-          );
-        }
-      }, [prop]);
-   
-    return (
-        <View style={styles.container}>
-            <View style={styles.column}>
-                <Text>{prop==null?"":prop.ColumnTitle}</Text>
-            </View>
-            <View style={[styles.column, styles.middleColumn]}>
-                <View style={styles.row}>
-                    <DropDownPicker
-                        open={open}
-                        value={dropdownValue}
-                        items={items}
-                        setOpen={setOpen}
-                        setValue={setDropdownValue}
-                        setItems={setItems}
-                        itemKey='label' 
+  const [dropdownValue, setDropdownValue] = useState(null);
+  const [dropdownLabel, setDropdownLabel] = useState(null);
 
-                        // containerStyle={styles.dropdownContainer}
-                        // style={styles.dropdown}
-                       
-                     
-                    //      onSelectItem={(value) => {
-                    // }}
-                    />
-                </View>
-                {/* <View style={styles.row}>
-                    <Text>Row 2</Text>
-                </View> */}
-            </View>
-            <View style={styles.column}>
-                <Text>{dropdownValue}</Text>
-            </View>
+  const handleValueChange = (itemValue,index) => {
+
+    setDropdownValue(itemValue);
+    setDropdownLabel(prop.ValueDescList[index].Descr);  
+    let obj = {ColumnName:prop.ColumnName,ValidValue:itemValue};
+    handleStagingCriteriaSelect(obj);
+  }
+ 
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.column, { flex: 1 }]}>
+        <Text>{prop == null ? '' : prop.ColumnTitle}</Text>
+      </View>
+      <View style={[styles.column, { flex: 3.5 }]}>
+        <View style={styles.DropdownPickerView}>
+          <Picker
+            style={styles.picker}
+            itemStyle={styles.dropdownItem}
+            selectedValue={dropdownValue==null ? '...' : dropdownValue}
+            onValueChange={(itemValue, itemIndex) => handleValueChange(itemValue,itemIndex)}
+            
+          >
+            {prop == null ? (
+              <Picker.Item label="loading..." value="..." />
+            ) : (
+              prop.ValueDescList.map((item, index) => (
+                <Picker.Item label={item.Descr} value={item.ValidValue} key={index} />
+              ))
+            )}
+          </Picker>
         </View>
-    );
+        <View style={[styles.row, styles.selectedRow]}>
+          <Text style={[{overflow:'auto'},{paddingRight:5,paddingLeft:5}]}>{dropdownLabel==null ? 'select criteria from dropdown above' : dropdownLabel}</Text>
+        </View>
+      </View>
+      <View style={[styles.column, { flex: .5 },{paddingRight:10},{paddingLeft:10}]}>
+        <Text>{dropdownValue}</Text>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-    },
-    column: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderWidth: 1,
-    },
-    middleColumn: {
-      borderLeftWidth: 0,
-      borderRightWidth: 0,
-    },
-    row: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-    
-    },
-    selectedRow: {
-      backgroundColor: '#e6f3ff',
-    },
-    dropdownContainer: {
-      width: '80%',
-      height: 40,
-    },
-    dropdown: {
-      backgroundColor: '#fafafa',
-    },
-    dropdownItem: {
-      justifyContent: 'flex-start',
-    },
-    dropdownMenu: {
-      marginTop: 40,
-      backgroundColor: '#fafafa',
-    },
-  });
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         flexDirection: 'row',
-//     },
-//     column: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         borderWidth: 1,
-//     },
-//     middleColumn: {
-//         borderLeftWidth: 0,
-//         borderRightWidth: 0,
-//     },
-//     row: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         borderWidth: 1,
-//     },
-// });
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 50,
+  },
+  column: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+  row: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: 100,
+  },
+  DropdownPickerView:{
+    width: '100%'
+  },
+  
+  selectedRow: {
+    backgroundColor: '#e6f3ff',
+  },
+  picker: {
+    width: '100%',
+    backgroundColor: '#fafafa',
+    flexWrap: 'wrap',
+  },
+  dropdownItem: {
+    justifyContent: 'flex-start',
+  },
+});
 
 export default StagingValueRow;
